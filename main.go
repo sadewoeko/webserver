d19427e507a0a4b5379c5b3fd2eb2bc96618a164
Pramesti Hatta K <opam.qweiop@gmail.com>
2018-11-25 00:19:18 +0700

    dasar

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const port = ":1234"
+
+func main() {
+	http.HandleFunc("/food", handlerFood)
+
+	fmt.Println("server started at port", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func handlerFood(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(r.URL.Query().Get("nomer")))
 }
